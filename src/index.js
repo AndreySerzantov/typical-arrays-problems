3a70d39f789b49bc00eb7fb3fe7734293cbfebe4
andrey <serzantovandreyiq@gmail.com>
2021-03-12 11:24:12 +0300

    feat: implemented the functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+let array = [1, 2, 3, 4];
 
 exports.min = function min (array) {
-  return 0;
+  let num = array[0];
+
+    for(let i of array){
+        num = Math.min(num, i);
+    }
+
+  return num;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let num = 0;
+
+    for(let i of arr){
+        num = Math.max(num, i);
+    }
+
+  return num;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+
+    for(let i of arr){
+        sum += i;
+    }
+
+  return sum / arr.length;
 }
